68a8e11d0cf7111a4b677b0c94fa1f55ebc4371c
TMGan <gangadeentim@gmail.com>
2025-06-11 10:54:56 -0400

    Initial commit

---
+++ src/main/java/com/coderscampus/TimAssignment10Application.java
@@ -0,0 +1,13 @@
+package com.coderscampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimAssignment10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimAssignment10Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/coderscampus/TimAssignment10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimAssignment10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
